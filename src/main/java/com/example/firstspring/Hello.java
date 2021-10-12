859cc79c0d2409f9c15613c0c47e5f45dbbc108f
赵宁 <1932303507@qq.com>
2021-10-12 19:09:08 +0800

    forstspring

--- src/main/java/com/example/firstspring/Hello.java
+++ src/main/java/com/example/firstspring/Hello.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello{
     @RequestMapping("/hello")
     public String Hello(){
-        return "hello spring world";
+        return "Hello Spring World";
     }
 }
