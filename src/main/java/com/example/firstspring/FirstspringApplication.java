eb4f8e24c7b4e4044770f3e22e849bc4a094c774
赵宁 <1932303507@qq.com>
2021-10-12 18:52:52 +0800

    forstspring

---
+++ src/main/java/com/example/firstspring/FirstspringApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FirstspringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/firstspring/Hello.java
@@ -0,0 +1,12 @@
+package com.example.firstspring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello{
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "hello spring world";
+    }
+}
